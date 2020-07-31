f5a09386215af01ac6e0c615c88455e911a80e7a
user175850 <user175850@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-07-31 14:48:12 -0400

    Implementing contact list

---
+++ Ndela054-UPEApp/Contacts.swift
@@ -0,0 +1,83 @@
+//
+//  Contacts.swift
+//  Ndela054-UPEApp
+//
+//  Created by user175850 on 7/31/20.
+//  Copyright © 2020 user175850. All rights reserved.
+//
+
+// Structure for contacts
+struct Contact
+{
+    var name: String
+    var email: String
+}
+
+// Protected database
+final class ContactDB
+{
+    static let sharedInstance = ContactDB()
+    
+    private var contactList: [Contact] = [Contact(name: "Andrew", email:"andrew@hotmail.com"),
+                                          Contact(name: "Carlos",email:"carlos@gmail.com"),
+                                          Contact(name: "Giselle",email:"giselle@outlook.com")]
+    
+    var displayIndex = -1
+    
+    private init()
+    {
+        sort()
+    }
+    
+    func nextContact() -> Contact
+    {
+        // Increment display index
+        displayIndex += 1
+        
+        // Test for wrap condition
+        if displayIndex >= contactList.count
+        {
+            displayIndex = 0
+        }
+        
+        // Return contact index
+        return contactList[displayIndex]
+    }
+    
+    func prevContact() -> Contact
+    {
+        // Decrement display index
+        displayIndex -= 1
+        
+        // Test for wrap condition
+        if displayIndex < 0
+        {
+            displayIndex = contactList.count-1
+        }
+        
+        // Return contact at index
+        return contactList[displayIndex]
+    }
+    
+    func updateContact(modified: Contact)
+    {
+        // Replace Contact info at displayIndex with modified
+        
+        contactList[displayIndex] = modified
+        
+        sort()
+    }
+    
+    func addContact(new: Contact)
+    {
+        // Append new to contactList
+        contactList.append(new)
+        
+        sort()
+    }
+    
+    private func sort()
+    {
+        contactList.sort(by: { $0.name < $1.name})
+    }
+}
