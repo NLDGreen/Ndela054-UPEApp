384eb544fa9b93c5108348f9db601135e831d91d
user175850 <user175850@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-08-01 22:39:05 -0400

    Calendar implemented?

---
+++ Ndela054-UPEApp/CalendarTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarTableCell.swift
+//  Ndela054-UPEApp
+//
+//  Created by user175850 on 8/1/20.
+//  Copyright © 2020 user175850. All rights reserved.
+//
+
+import UIKit
+
+class CalendarTableCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- Ndela054-UPEApp/CalendarViewController.swift
+++ Ndela054-UPEApp/CalendarViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class CalendarViewController: UITableViewController {
-
+class CalendarViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
+    
+    let tableview: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTableView()
     }
-
-
+    
+    
+    func setupTableView() {
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(CalendarTableCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableview.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! CalendarTableCell
+        cell.backgroundColor = UIColor.white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
-
